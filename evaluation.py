246a8380537545f012c0e532202ef36f0fb31cd0
Hyunseok Lim <loopback@office.ut.ac.kr>
2022-06-13 13:24:16 +0900

    Implement CRC_Check

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,8 @@
+import pandas as pd, json, numpy as np, nibabel as nib
+from glob import iglob
+from tqdm import tqdm
 from bidsio import BIDSLoader
-import pandas as pd
-import json
 from collections import defaultdict
-import numpy as np
 from multiprocessing import Pool
 from settings import eval_settings
 
@@ -93,6 +93,13 @@ def make_subloader_and_evaluate(data_list, data_shape, target_list, target_shape
     local_loader.target_shape = target_shape  # Needed since sample image is 1x1x1
     return evaluate(local_loader, eval_settings['ScoringFunctions'])
 
+def crc_check():
+    for path in tqdm(sorted(list(iglob('final_predictions_zip2/**/*.nii.gz', recursive=True)))):
+        try:
+            lbl = nib.load(path).get_fdata()
+        except Exception as e:
+            tqdm.write(path)
+            tqdm.write(e.__str__())
 
 if __name__ == "__main__":
     # Create data_description.json
