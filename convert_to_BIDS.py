569561cccc35aabdd83f577d9a04a26daa0e9b1d
Hyunseok Lim <loopback@office.ut.ac.kr>
2022-06-16 11:14:26 +0900

    Implement BIDS Conversion

--- convert_to_BIDS.py
+++ convert_to_BIDS.py
@@ -1,6 +1,29 @@
-import os
-import nibabel as nib
-import numpy as np
+import os, numpy as np, nibabel as nib
+from os.path import join, basename
+from tqdm import tqdm
+from glob import glob, iglob
+from bidsio import BIDSLoader
+from settings import eval_settings
+
+
+def chk_CRC(src_dir:str, fname:str='*', recursive:bool=True):
+    for path in tqdm(sorted(list(iglob(f'{src_dir}/{fname}', recursive={recursive}))), desc='CRC Checking'):
+        try:
+            _ = nib.load(path).get_fdata()
+        except Exception as e:
+            tqdm.write(path+': '+e.__str__())
+
+def convert_to_BIDS(src_dir:str, dst_dir:str, fname:str='*.nii.gz', recursive:bool=True):
+    for path in tqdm(sorted(list(iglob(join(src_dir, fname), recursive=recursive))), desc='Convert to BIDS', colour='green', dynamic_ncols=True):
+        # Directory configuration
+        first_dir = 'sub-r'+basename(path)[3:6]+'s'+basename(path)[7:10]
+        target_dir = join(dst_dir, first_dir, 'ses-1', 'anat')
+        os.makedirs(target_dir, exist_ok=True)
+        fname = join(f'{first_dir}_{"ses-1"}_{"space-MNI152NLin2009aSym_label_L_desc-T1lesion_mask.nii.gz"}')
+
+        # Save as NiBabel file        
+        nib.load(path).to_filename(join(target_dir, fname))
+
 
 path = os.getcwd() + '/output_Task505_ATLAS_OriginTest' #Check 1
 
@@ -26,3 +49,10 @@ for i in range(len(file_list_nii)):
     #fname = os.path.basename(file_list_nii1[i])
     refname = directory1+"_"+directory2+"_space-MNI152NLin2009aSym_label_L_desc-T1lesion_mask.nii.gz"
     img.to_filename(os.path.join(final_directory,'{}'.format(refname)))  # Save as NiBabel file
+
+if __name__ == '__main__':
+    # chk_CRC('output_Task507_ATLAS_ATLAS_3d_fullres_1000ep_10_0', fname='*.nii.gz')
+    # union()
+    convert_to_BIDS(model_prediction_dir, BIDS_formatted_dir)
+    BIDSLoader.write_dataset_description(BIDS_formatted_dir, eval_settings['PredictionBIDSDerivativeName'][0])
+    os.system(f'cd {BIDS_formatted_dir} && zip -qr ../{zip_filename} *')
